e0afa6e0a78020ee045157db1754d367dfe47e9b
ChyiYaqing <chyiyaqing@gmail.com>
2017-04-21 09:40:09 +0800

    build the first demo

---
+++ swiftSource/led-blink/Sources/main.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+// Get a dictionary of all the gpio pins
+let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
+
+// Get the pin that you connected the LED to. Remember to set the right pin number, for me it was P4
+guard let ledGpio = gpios[GPIOName.P4] else {
+  fatalError("Could not initialize the gpio")
+}
+
+ledGpio.direction = .OUT
+ledGpio.value = 0
+
+// Read user input from keyboard and switch the LED on/off each time the user presses the return key
+
+print("Press return to switch the LED on/off. To quit type exit")
+while let userInput = readLine(strippingNewline: true), userInput != "exit" {
+  print("Switching LED")
+  ledGpio.value = ledGpio.value == 0 ? 1 : 0
+}
